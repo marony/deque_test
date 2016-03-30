f37ae6219242635807858ec824dcc12ef2a33a41
marony <marony@binbo-kodakusan.com>
2016-03-30 18:24:44 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# C#の`List<T>`とQueue<T>の速度比較
+
+## 説明
+
+　C#(.Net Framework 4.5.2)の`List<T>`と`Queue<T>`を1億回後ろに追加し先頭を削除を繰り返してみました。  
+
+　文字列(参照型)のため`List<T>`でもオブジェクトのコピーは速いだろうと想像したのですが思ったよりも速度差がありました。`Queue<T>`の実装を見てみたところリングバッファになっているようで要素を追加してもノードオブジェクトのようなものは作られないのですね。  
+
+[queue.cs](http://referencesource.microsoft.com/#mscorlib/system/collections/queue.cs "queue.cs")
+
+## 結果
+
+List<T>での結果:  
+3226 millis  
+
+Queue<T>での結果:  
+2295 millis  
+
+1億回もコレクションに追加したり削除したりする機会はなかなかないと思いますが参考まで。
---
+++ deque_test/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace deque_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int N = 100000000;
+            const int MAX = 5;
+            {
+                string[] strings = { "ABC", "DEF", "GHI" };
+                var list = new List<string>();
+                var rand = new Random();
+
+                var sw = Stopwatch.StartNew();
+                for (var i = 0; i < N; ++i)
+                {
+                    list.Add(strings[rand.Next(strings.Length)]);
+                    if (list.Count > MAX)
+                        list.RemoveAt(0);
+                }
+                sw.Stop();
+                Console.WriteLine($"List<T> : count = {N}, {sw.ElapsedMilliseconds}ms");
+            }
+            {
+                string[] strings = { "ABC", "DEF", "GHI" };
+                var queue = new Queue<string>();
+                var rand = new Random();
+
+                var sw = Stopwatch.StartNew();
+                for (var i = 0; i < N; ++i)
+                {
+                    queue.Enqueue(strings[rand.Next(strings.Length)]);
+                    if (queue.Count > MAX)
+                        queue.Dequeue();
+                }
+                sw.Stop();
+                Console.WriteLine($"Queue<T> : count={N}, {sw.ElapsedMilliseconds}ms");
+            }
+            Console.WriteLine("[END]");
+            Console.ReadLine();
+        }
+    }
+}
